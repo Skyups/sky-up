5cee6a8b6e20a1eb31acf1ccd78a82ce556c757f
10Gaby <galvisgabriela1@gmail.com>
2022-05-15 20:56:11 -0500

    carga de archivos

---
+++ js/script.js
@@ -0,0 +1,80 @@
+$('#validate').click(function() {
+
+    var cambio = document.querySelector(".global-container");
+    var desc = document.querySelector(".desc");
+
+    if ($('input[value="a1"]').is(':checked') && $('input[value="a2"]').is(':checked')) {
+            cambio.classList.toggle("desaparecer");
+            desc.classList.toggle("aparecer");
+        var respuesta = document.querySelector("#R1");
+            respuesta.classList.toggle("aparecer");
+
+    }
+        else if ($('input[value="a1"]').is(':checked') && $('input[value="b2"]').is(':checked')) {
+            cambio.classList.toggle("desaparecer");
+            desc.classList.toggle("aparecer");
+        var respuesta = document.querySelector("#R2");
+            respuesta.classList.toggle("aparecer");
+
+        }
+        else if ($('input[value="a1"]').is(':checked') && $('input[value="c2"]').is(':checked')) {
+            cambio.classList.toggle("desaparecer");
+            desc.classList.toggle("aparecer");
+        var respuesta = document.querySelector("#R3");
+            respuesta.classList.toggle("aparecer");
+
+        }
+        else if ($('input[value="b1"]').is(':checked') && $('input[value="a2"]').is(':checked')) {
+            cambio.classList.toggle("desaparecer");
+            desc.classList.toggle("aparecer");
+        var respuesta = document.querySelector("#R4");
+            respuesta.classList.toggle("aparecer");
+
+        }
+            else if ($('input[value="b1"]').is(':checked') && $('input[value="b2"]').is(':checked')) {
+                cambio.classList.toggle("desaparecer");
+                desc.classList.toggle("aparecer");
+            var respuesta = document.querySelector("#R5");
+                respuesta.classList.toggle("aparecer");
+
+            }
+            else if ($('input[value="b1"]').is(':checked') && $('input[value="c2"]').is(':checked')) {
+                cambio.classList.toggle("desaparecer");
+                desc.classList.toggle("aparecer");
+            var respuesta = document.querySelector("#R6");
+                respuesta.classList.toggle("aparecer");
+
+            }
+                else if ($('input[value="c1"]').is(':checked') && $('input[value="a2"]').is(':checked')) {
+                    cambio.classList.toggle("desaparecer");
+                    desc.classList.toggle("aparecer");
+                var respuesta = document.querySelector("#R7");
+                    respuesta.classList.toggle("aparecer");
+
+                }
+                else if ($('input[value="c1"]').is(':checked') && $('input[value="b2"]').is(':checked')) {
+                    cambio.classList.toggle("desaparecer");
+                    desc.classList.toggle("aparecer");
+                var respuesta = document.querySelector("#R8");
+                    respuesta.classList.toggle("aparecer");
+
+                }
+                else if ($('input[value="c1"]').is(':checked') && $('input[value="c2"]').is(':checked')) {
+                    cambio.classList.toggle("desaparecer");
+                    desc.classList.toggle("aparecer");
+                var respuesta = document.querySelector("#R9");
+                    respuesta.classList.toggle("aparecer");
+
+                }
+
+    else if (!$('input[name="p1"]').is(':checked')) {
+        var aviso = document.getElementById('aviso');
+    aviso.innerHTML = '<p>Aún no has contestado la pregunta 1</p>';
+    }
+
+    else if (!$('input[name="p2"]').is(':checked')) {
+        var aviso = document.getElementById('aviso');
+    aviso.innerHTML = '<p>Aún no has contestado la pregunta 2</p>';
+    }
+   
+    });
